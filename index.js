d5c558cf650a78c988aefc19862caa72ba3c4636
nguyentruong17 <nguyentruong17@augustana.edu>
2020-06-04 15:16:22 -0500

    done: chapter 4

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
+const ejs = require('ejs') //helps to dynamically render HTML instead of just having static ones
 const app = new express()
 
+app.set('view engine', 'ejs') //teling express to use ejs as templating engine (any file ending with .ejs would be rendered with ejs engine)
 app.use(express.static('public')) //register a public folder in our static files
 
 app.listen(4000, ()=> {
@@ -9,16 +11,20 @@ app.listen(4000, ()=> {
 })
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    //res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index') //ejs engine will look into the folder 'views' and helps rendering the file index.ejs
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    //res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
 })
 
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    //res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
 })
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    //res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
 })
